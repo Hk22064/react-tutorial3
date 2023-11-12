50dc4f38c53882d4884c5dcd40482e794eb126cc
Hk22064 <146146098+Hk22064@users.noreply.github.com>
2023-11-12 22:26:51 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import "../style.css";
 
 export default function App() {
   const imagesData = [
-    { src: "../images/pic1.jpg", alt: "Closeup of a human eye" },
-    { src: "../images/pic2.jpg", alt: "Rock that looks like a wave" },
-    { src: "../images/pic3.jpg", alt: "Purple and white pansies" },
-    { src: "../images/pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
-    { src: "../images/pic5.jpg", alt: "Large moth on a leaf" }
+    { src: "images/pic1.jpg", alt: "Closeup of a human eye" },
+    { src: "images/pic2.jpg", alt: "Rock that looks like a wave" },
+    { src: "images/pic3.jpg", alt: "Purple and white pansies" },
+    { src: "images/pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+    { src: "images/pic5.jpg", alt: "Large moth on a leaf" }
   ];
 
   const [displayedImage, setDisplayedImage] = useState(imagesData[0]);
@@ -25,7 +25,7 @@ export default function App() {
   return (
     <>
       <h1>Image gallery example</h1>
-      <div className={`full-img ${isDarkened ? 'darkened' : ''}`}>
+      <div className={`full-img `}>
         <img
           className="displayed-img"
           src={displayedImage.src}
